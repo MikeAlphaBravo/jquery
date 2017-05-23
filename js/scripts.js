b3d4c3d248acdab56e722099610d6096fc67aa67
Camden Swita and Michael Brooks <camden@>
2017-05-22 18:26:40 -0700

    It puts the lotion on the skin.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paraphrase.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
